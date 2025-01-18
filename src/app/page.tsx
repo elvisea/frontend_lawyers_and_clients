32729f14ff493adff2709ae5d3912ff323c84e8b
Elvis <elvis.e.amancio@gmail.com>
2025-01-18 15:37:55 -0300

    added features section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,16 @@
 'use client'
 
-import Hero from '@/components/hero'
-import Princing from '@/components/pricing'
 import React from 'react'
 
-
+import Hero from '@/components/hero'
+import Features from '@/components/features'
+import Princing from '@/components/pricing'
 
 export default function Page() {
-
   return (
     <React.Fragment>
       <Hero />
+      <Features />
       <Princing />
     </React.Fragment>
   )
