72eeed1bca5ab38249e0e6459db147569a4e951a
Elvis <elvis.e.amancio@gmail.com>
2025-01-18 16:55:15 -0300

    added flows

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,14 +2,14 @@
 
 import React from 'react'
 
-import Hero from '@/components/hero'
-import Features from '@/components/features'
-import Princing from '@/components/pricing'
+import Hero from '@/components/sections/hero'
+import Features from '@/components/sections/features'
+import Princing from '@/components/sections/princing'
 
 export default function Page() {
   return (
     <React.Fragment>
-      <Hero />
+      <Hero resource='Clents and Lawyers' href='/contact' />
       <Features />
       <Princing />
     </React.Fragment>
--- src/components/sections/hero.tsx
+++ src/components/sections/hero.tsx
@@ -9,7 +9,12 @@ const navigation = [
   { name: 'Lawyers', href: '/lawyers' },
 ]
 
-export default function Hero() {
+type HeroProps = {
+  href: '/registration-client' | '/registration-lawyer' | '/contact';
+  resource: string;
+}
+
+export default function Hero({ resource, href }: HeroProps) {
   const [mobileMenuOpen, setMobileMenuOpen] = useState(false)
 
   return (
@@ -122,7 +127,7 @@ export default function Hero() {
           </div>
           <div className="text-center">
             <h1 className="text-balance text-5xl font-semibold tracking-tight text-gray-900 sm:text-7xl">
-              Data to enrich your online business
+              Data to enrich your online business ({resource})
             </h1>
             <p className="mt-8 text-pretty text-lg font-medium text-gray-500 sm:text-xl/8">
               Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo. Elit sunt amet
@@ -130,12 +135,12 @@ export default function Hero() {
             </p>
             <div className="mt-10 flex items-center justify-center gap-x-6">
               <a
-                href="#"
+                href={href}
                 className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
               >
                 Get started
               </a>
-              <a href="#" className="text-sm/6 font-semibold text-gray-900">
+              <a href={href} className="text-sm/6 font-semibold text-gray-900">
                 Learn more <span aria-hidden="true">→</span>
               </a>
             </div>
