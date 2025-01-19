993ab4d5af2b74b2e69dec23eacb4533488a2af9
Elvis <elvis.e.amancio@gmail.com>
2025-01-19 00:14:33 -0300

    altura padrão definida

--- src/components/input/index.tsx
+++ src/components/input/index.tsx
@@ -7,7 +7,7 @@ const Input: React.FC<InputProps> = ({ className, ...props }) => {
   return (
     <input
       className={classNames(
-        "block w-full rounded-md bg-white px-3.5 py-2 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600",
+        "block w-full rounded-md bg-white px-3.5 py-2 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 h-12",
         className
       )}
 
