d94e08e845e8ae5ae5b092eb25e30f546836955a
Elvis <elvis.e.amancio@gmail.com>
2025-01-18 22:12:54 -0300

    added components and function class-names

--- src/components/sections/contact.tsx
+++ src/components/sections/contact.tsx
@@ -4,6 +4,11 @@ import { useState } from 'react'
 import { Field, Label, Switch } from '@headlessui/react'
 import { ChevronDownIcon } from '@heroicons/react/16/solid'
 
+import InputLabel from '../label'
+import Input from '../input'
+import TextArea from '../text-area'
+import Button from '../button'
+
 type ContactProps = {
   resource: string;
 }
@@ -31,66 +36,30 @@ export default function Contact({ resource }: ContactProps) {
       </div>
       <form action="#" method="POST" className="mx-auto mt-16 max-w-xl sm:mt-20">
         <div className="grid grid-cols-1 gap-x-8 gap-y-6 sm:grid-cols-2">
+
           <div>
-            <label htmlFor="first-name" className="block text-sm/6 font-semibold text-gray-900">
-              First name
-            </label>
-            <div className="mt-2.5">
-              <input
-                id="first-name"
-                name="first-name"
-                type="text"
-                autoComplete="given-name"
-                className="block w-full rounded-md bg-white px-3.5 py-2 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600"
-              />
-            </div>
+            <InputLabel htmlFor="first-name" title='First name' />
+            <Input id="first-name" name="first-name" type="text" autoComplete="given-name" className="mt-2.5" />
           </div>
+
           <div>
-            <label htmlFor="last-name" className="block text-sm/6 font-semibold text-gray-900">
-              Last name
-            </label>
-            <div className="mt-2.5">
-              <input
-                id="last-name"
-                name="last-name"
-                type="text"
-                autoComplete="family-name"
-                className="block w-full rounded-md bg-white px-3.5 py-2 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600"
-              />
-            </div>
+            <InputLabel htmlFor="last-name" title='Last name' />
+            <Input id="last-name" name="last-name" type="text" autoComplete="family-name" className="mt-2.5" />
           </div>
+
           <div className="sm:col-span-2">
-            <label htmlFor="company" className="block text-sm/6 font-semibold text-gray-900">
-              Company
-            </label>
-            <div className="mt-2.5">
-              <input
-                id="company"
-                name="company"
-                type="text"
-                autoComplete="organization"
-                className="block w-full rounded-md bg-white px-3.5 py-2 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600"
-              />
-            </div>
+            <InputLabel htmlFor="company" title='Company' />
+            <Input id="company" name="company" type="text" autoComplete="organization" className="mt-2.5" />
           </div>
+
           <div className="sm:col-span-2">
-            <label htmlFor="email" className="block text-sm/6 font-semibold text-gray-900">
-              Email
-            </label>
-            <div className="mt-2.5">
-              <input
-                id="email"
-                name="email"
-                type="email"
-                autoComplete="email"
-                className="block w-full rounded-md bg-white px-3.5 py-2 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600"
-              />
-            </div>
+            <InputLabel htmlFor="email" title='Email' />
+            <Input id="email" name="email" type="email" autoComplete="email" className="mt-2.5" />
           </div>
+
           <div className="sm:col-span-2">
-            <label htmlFor="phone-number" className="block text-sm/6 font-semibold text-gray-900">
-              Phone number
-            </label>
+            <InputLabel htmlFor="phone-number" title='Phone number' />
+
             <div className="mt-2.5">
               <div className="flex rounded-md bg-white outline outline-1 -outline-offset-1 outline-gray-300 has-[input:focus-within]:outline has-[input:focus-within]:outline-2 has-[input:focus-within]:-outline-offset-2 has-[input:focus-within]:outline-indigo-600">
                 <div className="grid shrink-0 grid-cols-1 focus-within:relative">
@@ -110,6 +79,7 @@ export default function Contact({ resource }: ContactProps) {
                     className="pointer-events-none col-start-1 row-start-1 mr-2 size-5 self-center justify-self-end text-gray-500 sm:size-4"
                   />
                 </div>
+
                 <input
                   id="phone-number"
                   name="phone-number"
@@ -117,23 +87,16 @@ export default function Contact({ resource }: ContactProps) {
                   placeholder="123-456-7890"
                   className="block min-w-0 grow py-1.5 pl-1 pr-3 text-base text-gray-900 placeholder:text-gray-400 focus:outline focus:outline-0 sm:text-sm/6"
                 />
+
               </div>
             </div>
           </div>
+
           <div className="sm:col-span-2">
-            <label htmlFor="message" className="block text-sm/6 font-semibold text-gray-900">
-              Message
-            </label>
-            <div className="mt-2.5">
-              <textarea
-                id="message"
-                name="message"
-                rows={4}
-                className="block w-full rounded-md bg-white px-3.5 py-2 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600"
-                defaultValue={''}
-              />
-            </div>
+            <InputLabel htmlFor="message" title='Message' />
+            <TextArea id="message" name="message" rows={4} defaultValue={''} className="mt-2.5" />
           </div>
+
           <Field className="flex gap-x-4 sm:col-span-2">
             <div className="flex h-6 items-center">
               <Switch
@@ -157,14 +120,7 @@ export default function Contact({ resource }: ContactProps) {
             </Label>
           </Field>
         </div>
-        <div className="mt-10">
-          <button
-            type="submit"
-            className="block w-full rounded-md bg-indigo-600 px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-          >
-            Lets talk
-          </button>
-        </div>
+        <Button title='Lets talk' className="mt-10" />
       </form>
     </div>
   )
