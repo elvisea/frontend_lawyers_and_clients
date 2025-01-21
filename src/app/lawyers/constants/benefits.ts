b95925682eb34a3b070e5cfb198b869b621f20f0
Elvis <elvis.e.amancio@gmail.com>
2025-01-21 15:51:00 -0300

    added section

---
+++ src/app/lawyers/constants/benefits.ts
@@ -0,0 +1,28 @@
+import { CloudArrowUpIcon, LockClosedIcon, ServerIcon, ShieldCheckIcon } from '@heroicons/react/24/outline'
+
+export const benefits = [
+  {
+    name: 'Confidencialidade',
+    description:
+      'Seus dados e documentos são protegidos.',
+    icon: CloudArrowUpIcon,
+  },
+  {
+    name: 'Rapidez',
+    description:
+      'Encontre ajuda jurídica sem complicações.',
+    icon: ShieldCheckIcon,
+  },
+  {
+    name: 'Profissionais Qualificados',
+    description:
+      'Advogados avaliados e experientes.',
+    icon: ServerIcon,
+  },
+  {
+    name: 'Simples e Intuitivo',
+    description:
+      'Processo descomplicado, ideal para todos.',
+    icon: LockClosedIcon,
+  },
+]
--- src/app/lawyers/page.tsx
+++ src/app/lawyers/page.tsx
@@ -2,6 +2,9 @@
 
 import React from 'react'
 
+import { texts } from './constants/texts'
+import { features } from './constants/features'
+
 import Hero from '@/components/sections/hero'
 import Features from '@/components/sections/features'
 import Princing from '@/components/sections/princing'
@@ -9,8 +12,19 @@ import Princing from '@/components/sections/princing'
 export default function Lawyers() {
   return (
     <React.Fragment>
-      <Hero resource='Lawyers' href='/register-lawyer' />
-      <Features />
+      <Hero
+        resource='Lawyers'
+        href='/register-lawyer'
+        title={texts.hero.title}
+        description={texts.hero.description}
+        button={texts.hero.button}
+      />
+      <Features
+        miniTitle={texts.features.miniTitle}
+        title={texts.features.title}
+        description={texts.features.description}
+        features={features}
+      />
       <Princing />
     </React.Fragment>
   )
